3bf3170f29408bbe535632da1f6f809aa4f25678
Ozan GUL <ozan.gul@outlook.com.tr>
2025-04-17 13:13:03 -0400

    Loop Exercises

---
+++ LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 10; i>=1; i--)
+        {
+            System.out.println(i);
+    }
+}
+}
